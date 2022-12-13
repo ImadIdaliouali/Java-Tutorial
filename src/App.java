b84dcf48a4e60c56fa72c04158ee1275c4c1f057
imad <imadidaliouali@gmail.com>
2022-12-13 21:52:57 +0100

    For Loop

--- src/App.java
+++ src/App.java
@@ -1,9 +1,7 @@
 public class App {
     public static void main(String[] args) {
-        int n = 5;
-        do {
+        for (int n = 5; n >= 0; n--) {
             System.out.println("n = " + n);
-            n--;
-        } while (n >= 0);
+        }
     }
 }
