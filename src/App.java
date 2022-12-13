5f4d6b959a006e3a4e050261e0ab6368eb85e78c
imad <imadidaliouali@gmail.com>
2022-12-13 21:43:13 +0100

    Switch Statement

--- src/App.java
+++ src/App.java
@@ -1,13 +1,24 @@
 public class App {
     public static void main(String[] args) {
-        int a = 5;
-        if (a > 5) {
-            a++;
-        } else if (a > 2) {
-            a--;
-        } else {
-            a *= 2;
+        int a = 3;
+        switch (a) {
+            case 1:
+                System.out.println("a = 1");
+                break;
+            case 2:
+                System.out.println("a = 2");
+                break;
+            case 3:
+                System.out.println("a = 3");
+                break;
+            case 4:
+                System.out.println("a = 4");
+                break;
+            case 5:
+                System.out.println("a = 5");
+                break;
+            default:
+                System.out.println("a is not between 1 and 5");
         }
-        System.out.println("a = " + a);
     }
 }
