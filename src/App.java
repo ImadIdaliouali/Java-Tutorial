6004e9e9dd9fb41f4a2309e92bf4964579e36d1e
imad <imadidaliouali@gmail.com>
2022-12-13 21:16:21 +0100

    ? operator

--- src/App.java
+++ src/App.java
@@ -1,8 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        int n = 123;
-        String s = Integer.toString(n);
-        int i = Integer.parseInt(s);
-        System.out.println("n = " + n + ",s = " + s + ", i = " + i);
+        int n = 1;
+        System.out.println(n == 1 ? true : false);
     }
 }
