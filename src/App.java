c7eca8152a0a1f7be360857741e8d226c4f93db1
imad <imadidaliouali@gmail.com>
2022-12-13 21:47:00 +0100

    While Loop

--- src/App.java
+++ src/App.java
@@ -1,24 +1,9 @@
 public class App {
     public static void main(String[] args) {
-        int a = 3;
-        switch (a) {
-            case 1:
-                System.out.println("a = 1");
-                break;
-            case 2:
-                System.out.println("a = 2");
-                break;
-            case 3:
-                System.out.println("a = 3");
-                break;
-            case 4:
-                System.out.println("a = 4");
-                break;
-            case 5:
-                System.out.println("a = 5");
-                break;
-            default:
-                System.out.println("a is not between 1 and 5");
+        int i = 0;
+        while (i < 5) {
+            System.out.println("i = " + i);
+            i++;
         }
     }
 }
