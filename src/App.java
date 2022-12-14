55c31822d474b71cbc24c8606bd306b1d5424d18
imad <imadidaliouali@gmail.com>
2022-12-14 09:00:53 +0100

    Classes/Objects

--- src/App.java
+++ src/App.java
@@ -1,11 +1,11 @@
 public class App {
     public static void main(String[] args) {
-        String name = "imad";
-        String nom = "imad";
-        System.out.println(name.equals(nom) ? "ok" : "not ok");
-        for (int i = 0; i < name.length(); i++) {
-            System.out.println(name.charAt(i));
-            System.out.println(name.indexOf(name.charAt(i)));
-        }
+        Car myCar = new Car();
+        Car otherCar = new Car();
+        myCar.year = 2022;
+        otherCar = myCar;
+        otherCar.year = 2010;
+        System.out.println(myCar.year);
+        System.out.println(otherCar.year);
     }
 }
