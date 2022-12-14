59bd3e4d1cd38c790937d4a642d857bcbe5d0e83
imad <imadidaliouali@gmail.com>
2022-12-14 08:41:24 +0100

    array equality

--- src/App.java
+++ src/App.java
@@ -1,7 +1,13 @@
 public class App {
     public static void main(String[] args) {
-        for (int n = 5; n >= 0; n--) {
-            System.out.println("n = " + n);
-        }
+        char[] c1 = { 'i', 'm', 'a', 'd' };
+        char[] c2 = { 'i', 'm', 'a', 'd' };
+        char[] c3 = c1;
+        if (c1 == c2)
+            System.out.println("c1 == c2");
+        if (c1 == c3)
+            System.out.println("c1 == c3");
+        if (c2 == c3)
+            System.out.println("c2 == c3");
     }
 }
